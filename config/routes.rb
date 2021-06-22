fc18752bd5b6e3d57e047288a163c7c88000b526
John Christopher F. Angeles <jfangeles2@up.edu.ph>
2021-06-22 18:32:03 +0800

    Added Rendering Partials -Made the text center in about us and home pages -Modified the bootstrap version to 4.6 to make the NavBar responsiveness work -Added the partial for the navbar and replaced anchor tags with rails helper link_to -Changed the route for 'about' to 'about-us' and renamed it to 'about' using the 'as: '

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "about", to: "about#index"
+  get "about-us", to: "about#index", as: :about
   # get is the method to be used
-  # "about" is the route eg.com/about
+  # "about-us" is the route eg.com/about-us
   # "about#index" looks for a controller in ./app/controllers with the same name. In this example, it should be named 'about_controller.rb'. This is the naming convention for the controllers. Here, index is the name of the action/method of the class AboutController which specifies the action.
+  # as: renames the route about-us to about. This is helpful when changing route names so you will not need to edit all the links to the path about-us
+  # This is mainly used with Rails helpers link_to which is seen in _navbar.html.erb in './app/views/shared/'
 
   # get "/", to: "main#index"
   root to:"main#index"
